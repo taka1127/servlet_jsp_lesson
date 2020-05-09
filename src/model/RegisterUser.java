e7fa0b27133842ae77541f19ca43210b3618197b
taka1127 <taka07251127@gmail.com>
2020-05-09 23:49:26 +0900

    20200509

---
+++ src/model/RegisterUser.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/RegisterUser")
+public class RegisterUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//フォワード先
+		String forwardPath = null;
+
+		//サーブレットクラスの動作を決定するactionの値をリクエストパラメータから取得
+		String action = request.getParameter("action");
+
+		//リクエストされた時の分岐
+		if(action==null) {//登録開始の場合
+			forwardPath = "/WEB-INF/jsp/registerForm.jsp";
+		}else if(action.equals("done")){//登録確認から登録実行の時
+			//セッションスコープに保存された登録ユーザーを取得
+			HttpSession session = request.getSession();
+			User registerUser = (User)session.getAttribute("registerUser");
+
+			//登録処理の呼び出し
+			RegisterUserLogic logic = new RegisterUserLogic();
+			logic.excute(registerUser);
+
+			//不要になったセッションスコープ内のインスタンスを削除
+			session.removeAttribute("registerUser");
+
+			//登録後のフォワード先を設定
+			forwardPath = "/WEB-INF/jsp/registerDone.jsp";
+
+		}
+
+		//設定されたフォワード先にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher(forwardPath);
+		dispatcher.forward(request, response);
+
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//リクエストパラメータの取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+
+		//登録するユーザー情報を設定
+		User registerUser = new User(id, name, pass);
+
+		//セッションスコープに登録ユーザーを保存
+		HttpSession session = request.getSession();
+		session.setAttribute("registerUser", registerUser);
+	}
+
+}

b1fb15323da4acd45f3a2280ec948635757aefdf
taka1127 <taka07251127@gmail.com>
2020-05-09 18:11:48 +0900

    20200509_01

---
+++ WebContent/WEB-INF/ex/fruit.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="ex.Fruit" %>
+<% Fruit f = (Fruit)request.getAttribute("fruit"); %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<p><%= f.getName() %>の値段は<%= f.getPrice() %></p>
+<p><%= f.getClass() %></p>
+</body>
+</html>
---
+++ src/ex/FruitServlet.java
@@ -0,0 +1,26 @@
+package ex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FruitServlet")
+public class FruitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Fruit f = new Fruit("イチゴ", 700);
+		request.setAttribute("fruit", f);
+		RequestDispatcher d = request.getRequestDispatcher("WEB-INF/ex/fruit.jsp");
+		d.forward(request, response);
+	}
+
+}
